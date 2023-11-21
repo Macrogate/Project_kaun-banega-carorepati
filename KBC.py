9055593e292bf4a786d375b1193e805f2d276afd
sumit <115890886+Macrogate@users.noreply.github.com>
2023-11-21 11:39:02 +0530

    Add files via upload

---
+++ KBC.py
@@ -0,0 +1,317 @@
+print("----------WELCOME TO KAUN BANEGA CROREPATI----------")
+print("-------------------WITH MR:SUMIT--------------------")
+print("Do you want to play")
+A=input("say: 'yes or no'")
+if(A=="yes"):
+  
+  print(" ")
+  print("you have to give the anwers of the following quetions:___")
+  B=input("Do you understand say'yes or no'")
+  if(B=='yes'):
+    
+    print(" \n")
+    print('SO, rules of the game are:\n ')
+    print("1. There are 20 questios in this game:--")
+    print("2. You have to give the answer of thee questions:--")
+    print("3. Each question have a given amounrt of money:--")
+    print("4. each right answer give you given amount of money:--")
+    print("5. There ae 4 options in each quesions:--")
+    print("6. Among them 1 is correct:--")
+    print("\n")
+    C=input("Do you understand say 'yes or no'")
+    if(C=="yes"):
+      
+      print("Now the questions Prie are:----")
+      print("1. Que Prize Is:: 5,000/-")
+      print("2. Que Prize Is:: 10,000/-")
+      print("3. Que Prize Is:: 15,000/-")
+      print("4. Que Prize Is:: 20,000/-")
+      print("5. Que Prize Is:: 25,000/-")
+      print("6. Que Prize Is:: 30,000/-")
+      print("7. Que Prize Is:: 35,000/-")
+      print("8. Que Prize Is:: 40,000/-")
+      print("9. Que Prize Is:: 45,000/-")
+      print("10. Que Prize Is:: 50,000/-")
+      print("11. Que Prize Is:: 55,000/-")
+      print("12. Que Prize Is:: 60,000/-")
+      print("13. Que Prize Is:: 65,000/-")
+      print("14. Que Prize Is:: 1,00,000/-")
+      print("15. Que Prize Is:: 5,00,000/-")
+      print("16. Que Prize Is:: 7,00,000/-")
+      print("17. Que Prize Is:: 20,00,000/-")
+      print("18. Que Prize Is:: 50,00,000/-")
+      print("19. Que Prize Is:: 1,00,00000/-")
+      print("20. Que Prize Is:: 10,000,0000/-")
+    else:
+      
+      print("Read again:--")
+      print("1. Que Prize Is:: 5,000/-")
+      print("2. Que Prize Is:: 10,000/-")
+      print("3. Que Prize Is:: 15,000/-")
+      print("4. Que Prize Is:: 20,000/-")
+      print("5. Que Prize Is:: 25,000/-")
+      print("6. Que Prize Is:: 30,000/-")
+      print("7. Que Prize Is:: 35,000/-")
+      print("8. Que Prize Is:: 40,000/-")
+      print("9. Que Prize Is:: 45,000/-")
+      print("10. Que Prize Is:: 50,000/-")
+      print("11. Que Prize Is:: 55,000/-")
+      print("12. Que Prize Is:: 60,000/-")
+      print("13. Que Prize Is:: 65,000/-")
+      print("14. Que Prize Is:: 1,00,000/-")
+      print("15. Que Prize Is:: 5,00,000/-")
+      print("16. Que Prize Is:: 7,00,000/-")
+      print("17. Que Prize Is:: 20,00,000/-")
+      print("18. Que Prize Is:: 50,00,000/-")
+      print("19. Que Prize Is:: 1,00,00000/-")
+      print("20. Que Prize Is:: 10,000,0000/-")
+      print(" \n")
+    print("LETS GET STARTED:--")
+    print("GET REDY FOR THE FIRST QUESTION:--")
+    print(" \n")
+    print("---------------------------------------------------------------------------")
+    print("                1. WEB CRAWLER IS AN EXAMPLE OF--")
+    print(" \n")
+    print('''        1.intelligent agent        2.problemsolvinf agent                       3.simple reflex agent      4.model based agent''')
+    D=input("               enter your answer:")
+    print(" \n")
+    if(D=='1'):
+      
+      print("               Correct answer🎉🎉🎉🎉🎉🎉🎉🎉")
+      print(" \n")
+      print("-------------------------------------------------------------------------")
+      print("            2. Automatic resoning tool is used in--")
+      print(" \n")
+      print('''        1.personal computers              2.Microcomputers                      3.LISP Machines                   4.All of the abve''')
+      E=input("               enter your answer:")
+      print(" \n")
+      if(E=='3'):
+        
+        print("               Correct answer🎉🎉🎉🎉🎉🎉🎉🎉")
+        print(" \n")
+        print("-------------------------------------------------------------------------")
+        print('''          3. which AI technique enables the computers to
+          understand theassociations and relationship
+          between onjects and events---''')
+        print(" \n")
+        print('''        1.Heuristic processing             2.cognitive procesing                      3.relative symbolizing                   4.pattern machine''')
+        F=input("               enter your answer:")
+        print(" \n")
+        if(F=='4'):
+          
+          print("               Correct answer🎉🎉🎉🎉🎉🎉🎉🎉")
+          print(" \n")
+          print("-------------------------------------------------------------------------")
+          print('''          4. Ways to achieve AI in real life are---''')
+          print(" \n")
+          print('''        1.Machine Learning             2.Deep Learning                      3.Boat a and b                 4.None of the above''')
+          G=input("               enter your answer:")
+          print(" \n")
+          if(G=='3'):
+            
+            print("               correct answer🎉🎉🎉🎉🎉🎉🎉🎉")
+            print(" \n")
+            print("-------------------------------------------------------------------------")
+            print('''          5. Which programminG langage is used for develoing  android apps---''')
+            print(" \n")
+            print('''        1.java             2.C#                     3.Python                   4.Swit''')
+            H=input("               enter your answer:")
+            print(" \n")
+            if(H=='1'):
+              
+              print("               Correct answer🎉🎉🎉🎉🎉🎉🎉🎉")
+              print(" \n")
+              print("-------------------------------------------------------------------------")
+              print('''          6. which data sructure is used for storing a cpllestion of elemsnts in a non-linear fashion---''')
+              print(" \n")
+              print('''        1.array             2.stack                     3.queue                   4.tree''')
+              I=input("               enter your answer:")
+              print(" \n")
+              if(I=='4'):
+                
+                print("               Correct answer🎉🎉🎉🎉🎉🎉🎉🎉")
+                print(" \n")
+                print("-------------------------------------------------------------------------")
+                print('''          7. Which of the following is ot a data type in most of the programming languages---''')
+                print(" \n")
+                print('''        1.integer             2.float                     3.double                   4.string''')
+                J=input("               enter your answer:")
+                print(" \n")
+                if(J=='3'):
+                  
+                  print("               Correct answer🎉🎉🎉🎉🎉🎉🎉🎉")
+                  print(" \n")
+                  print("-------------------------------------------------------------------------")
+                  print('''          8. which of the following are calles Ket industrial animals---''')
+                  print(" \n")
+                  print('''        1.producers             2.Tertiaryb consumers                     3.Primary consumers       4.None of the above''')
+                  K=input("               enter your answer:")
+                  print(" \n")
+                  if(K=='3'):
+                    
+                    print("               Correct answer🎉🎉🎉🎉🎉🎉🎉🎉")
+                    print(" \n")
+                    print("-------------------------------------------------------------------------")
+                    print('''          9. which of the given device is used for counting blood cells---''')
+                    print(" \n")
+                    print('''        1.Hmelethometer             2.Spyscometer                     3.Hemocymeter                   4.Hamosymeter''')
+                    L=input("               enter your answer:")
+                    print(" \n")
+                    if(L=='3'):
+                      
+                      print("               Correct answer🎉🎉🎉🎉🎉🎉🎉🎉")
+                      print(" \n")
+                      print("-------------------------------------------------------------------------")
+                      print('''          10. Which among the following was rhe forst indian women who went into space---''')
+                      print(" \n")
+                      print('''        1.Klpna chawla             2.Sunita williams                    3.Koneru Humpy         4.None of the aove''')
+                      M=input("               enter your answer:")
+                      print(" \n")
+                      if(M=='1'):
+                        
+                        print("               Correct answer🎉🎉🎉🎉🎉🎉🎉🎉")
+                        print(" \n")
+                        print("-------------------------------------------------------------------------")
+                        print('''          11. Who was the first indian to go into sapce---''')
+                        print(" \n")
+                        print('''        1.Vikram Ambala             2.Ravish Malhotra                   3.Rakesh sharma         4.Nagapathi Bhat''')
+                        N=input("               enter your answer:")
+                        print(" \n")
+                        if(N=='3'):
+                          
+                          print("               Correct answer🎉🎉🎉🎉🎉🎉🎉🎉")
+                          print(" \n")
+                          print("-------------------------------------------------------------------------")
+                          print('''          12. WHo was the first indin scientist to win a Noble Prize---''')
+                          print(" \n")
+                          print('''        1.C.V Raman             2.Amaryta Sen                3.Hargobind Khorana                   4.J.J Watson''')
+                          P=input("               enter your answer:")
+                          print(" \n")
+                          if(P=='1'):
+                            
+                            print("               Correct answer🎉🎉🎉🎉🎉🎉🎉🎉")
+                            print(" \n")
+                            print("-------------------------------------------------------------------------")
+                            print('''          13. Who is Mrs.World 2023---''')
+                            print(" \n")
+                            print('''        1.Aliya Syeda             2.Premila Naidu                   3.Sargam Koushal          4.Hemangi Sonit''')
+                            Q=input    ("               enter your answer:")
+                            print(" \n")
+                            if(Q=='3'):
+                              
+                              print("               Correct answer🎉🎉🎉🎉🎉🎉🎉🎉")
+                              print(" \n")
+                              print("-------------------------------------------------------------------------")
+                              print('''          14. Who is the richest person on Forbes 2022's List---''')
+                              print(" \n")
+                              print('''        1.Elon Musk             2.Jeff Bezos              3.Larry ellison                   4.Mukesh Ambani''')
+                              R=input("               enter your answer:")
+                              print(" \n")
+                              if(R=='1'):
+                                
+                                print("               Correct answer🎉🎉🎉🎉🎉🎉🎉🎉")
+                                print(" \n")
+                                print("-------------------------------------------------------------------------")
+                                print('''          15. Who invented the ball point pen---''')
+                                print(" \n")
+                                print('''        1.Biro Bhothers          2.Waterman Brothers              3.Bicc Brothers        4.Write Brothers''')
+                                S=input    ("               enter your answer:")
+                                print(" \n")
+                                if(S=='1'):
+                                  
+                                  print("               Correct answer🎉🎉🎉🎉🎉🎉🎉🎉")
+                                  print(" \n")
+                                  print("-------------------------------------------------------------------------")
+                                  print('''          16. What invetion cause many death while testing it---''')
+                                  print(" \n")
+                                  print('''        1.Dynamite             2.Ladders                     3.Race Cars                   4.Parachute''')
+                                  T=input("               enter your answer:")
+                                  print(" \n")
+                                  if(T=='4'):
+                                    
+                                    print("               Correct answer🎉🎉🎉🎉🎉🎉🎉🎉")
+                                    print(" \n")
+                                    print("-------------------------------------------------------------------------")
+                                    print('''         17. Artificial intelligency is about---''')
+                                    print(" \n")
+                                    print('''        1.game          2.Make a machine intelligent            3.robot        4.Automatic innovtions''')
+                                    U=input    ("               enter your answer:")
+                                    print(" \n")
+                                    if(U=='2'):
+                                      
+                                      print("               Correct answer🎉🎉🎉🎉🎉🎉🎉🎉")
+                                      print(" \n")
+                                      print("-------------------------------------------------------------------------")
+                                      print('''          18. who is Known as the father of AI---''')
+                                      print(" \n")
+                                      print('''        1.Ficher Ada           2.Alan Turing             3.John Mccarthy            4.Allen Newell''')
+                                      V=input("               enter your answer:")
+                                      print(" \n")
+                                      if(V=='3'):
+                                        
+                                        print("               Correct answer🎉🎉🎉🎉🎉🎉🎉🎉")
+                                        print(" \n")
+                                        print("-------------------------------------------------------------------------")
+                                        print('''          19. Which of the language is not commonly used for AI---''')
+                                        print(" \n")
+                                        print('''        1.LISP          2.PROLOG              3.Python        4.Perl''')
+                                        W=input    ("               enter your answer:")
+                                        print(" \n")
+                                        if(W=='4'):
+                                          
+                                          print("               Correct answer🎉🎉🎉🎉🎉🎉🎉🎉")
+                                          print(" \n")
+                                          print("-------------------------------------------------------------------------")
+                                          print('''          20. The available ways to sove the proble of state-space-search---''')
+                                          print(" \n")
+                                          print('''        1.one             2.two             3.three            4.four''')
+                                          W=input("               enter your answer:")
+                                          print(" \n")
+                                          print("-----------------------------------------------------------------------------")
+                                          print(" YOU WON THE KBC AND YOUR WINING AMOUNT IS 10,00,00,000Rs ONLY 🎉🎉🎉🎉🎉🎉🎉🎉")
+                                          print("-----------------------------------------------------------------------------")
+                                          print("\n")
+                                        else:
+                                          
+                                          print(" YOU LOST THE KBC 😥😥😥😥  AND YOUR WINNING AMOUNT IS 1,00,00,000Rs ONLY")
+                                      else:
+                                        
+                                        print(" YOU LOST THE KBC 😥😥😥😥  AND YOUR WINNING AMOUNT IS 50,00,000Rs ONLY")
+                                    else:
+                                      print(" YOU LOST THE KBC 😥😥😥😥  AND YOUR WINNING AMOUNT IS 20,00,000Rs ONLY")
+                                  else:
+                                    print(" YOU LOST THE KBC 😥😥😥😥  AND YOUR WINNING AMOUNT IS 7,00,000Rs ONLY")
+                                else:
+                                  print(" YOU LOST THE KBC 😥😥😥😥  AND YOUR WINNING AMOUNT IS 5,00,000Rs ONLY")
+                              else:
+                                print(" YOU LOST THE KBC 😥😥😥😥  AND YOUR WINNING AMOUNT IS 1,00,000Rs ONLY")
+                            else:
+                              print(" YOU LOST THE KBC 😥😥😥😥  AND YOUR WINNING AMOUNT IS 65,000Rs ONLY")
+                          else:
+                            print(" YOU LOST THE KBC 😥😥😥😥  AND YOUR WINNING AMOUNT IS 60,000Rs ONLY")
+                        else:
+                          print(" YOU LOST THE KBC 😥😥😥😥  AND YOUR WINNING AMOUNT IS 55,000Rs ONLY")
+                      else:
+                        print(" YOU LOST THE KBC 😥😥😥😥  AND YOUR WINNING AMOUNT IS 50,000Rs ONLY")
+                    else:
+                      print(" YOU LOST THE KBC 😥😥😥😥  AND YOUR WINNING AMOUNT IS 45,000Rs ONLY")
+                  else:
+                    print(" YOU LOST THE KBC 😥😥😥😥  AND YOUR WINNING AMOUNT IS 40,000Rs ONLY")
+                else:
+                  print(" YOU LOST THE KBC 😥😥😥😥  AND YOUR WINNING AMOUNT IS 35,000Rs ONLY")
+              else:
+                print(" YOU LOST THE KBC 😥😥😥😥  AND YOUR WINNING AMOUNT IS 30,000Rs ONLY")
+            else:
+              print(" YOU LOST THE KBC 😥😥😥😥  AND YOUR WINNING AMOUNT IS 25,000Rs ONLY")
+          else:
+            print(" YOU LOST THE KBC 😥😥😥😥  AND YOUR WINNING AMOUNT IS 20,000Rs ONLY")
+        else:
+          print(" YOU LOST THE KBC 😥😥😥😥  AND YOUR WINNING AMOUNT IS 15,000Rs ONLY")
+      else:
+        print(" YOU LOST THE KBC 😥😥😥😥  AND YOUR WINNING AMOUNT IS 10,000Rs ONLY")
+    else:
+      print("                      SORRY! YOU LOST THE GAME 😥😥😥😥")
+  else:
+    print("                    OK NO PROBLEM")
+else:
+  print("---------------------------GAME OVER---------------------------------- ")                             
